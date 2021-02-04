e3a28c7cc7c4d17ec352741823f352fbe2b015f8
xaviguasch <xaviguasch@gmail.com>
2021-02-04 12:24:44 +0100

    solved exercise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('testing!!!!!!')
+// Math.random()
+// Math.floor() - rounds numbers down to the nearest integer
+// Math.ceil() - round up
+// Math.round() - round to nearest integer
+
+const drinksMenu = ['Vodka Tonic', 'White Wine', 'Beer', 'Mocktail']
+
+const randomDrink = drinksMenu[Math.floor(Math.random() * drinksMenu.length)]
+
+console.log(randomDrink)
